1957174376afa79cd04207f70be6dac8ffdb2681
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-06 14:05:34 +0300

    <Backend> Add: stories table

---
+++ backend/database/migrations/2025_05_06_105520_create_stories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->foreignId('martyr_id')->nullable()->constrained('martyrs')->onDelete('cascade');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->text('content')->nullable();
+            $table->boolean('updating')->default(false);
+            $table->boolean('isUpdated')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stories');
+    }
+};
