c23dbf1b2bdc365921d9adf52b693878e7963dc8
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-06 20:02:13 +0300

    <Backend> Fix: foreignId for users table

--- backend/database/migrations/2025_05_06_160637_add_foreign_keys_to_users_table.php
+++ backend/database/migrations/2025_05_06_160637_add_foreign_keys_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->constrained('role');
+            $table->foreignId('role_id')->constrained('roles');
         });
     }
 
