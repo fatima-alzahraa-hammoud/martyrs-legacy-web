1b7d0047da246c7f655ca9a72d9cc33043fd2249
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-06 14:48:20 +0300

    <Backend> Add: martyr_wills table

---
+++ backend/database/migrations/2025_05_06_112309_create_martyr_wills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('martyr_wills', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('martyr_id')->nullable()->constrained('martyrs')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->enum('document_type', ['will', 'letter', 'audio_message', 'video_message']);
+            $table->foreignId('video_id')->constrained('videos');
+            $table->string('file_path'); // For documents/audio
+            $table->text('content')->nullable(); // For text transcription
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wills');
+    }
+};
