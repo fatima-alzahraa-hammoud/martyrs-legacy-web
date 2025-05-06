079ccf5b486b080f96aed7e2ecb3fa82b6ba7b80
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-06 18:42:06 +0300

    <Backend> Add: speeches table

---
+++ backend/database/migrations/2025_05_06_153414_create_speeches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('speeches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('martyr_id')->constrained('martyrs')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title');
+            $table->text('description');
+            $table->enum('document_type', ['speech', 'letter', 'audio_message', 'video_message']);
+            $table->foreignId('video_id')->nullable()->constrained('medias');
+            $table->foreignId('audio_id')->nullable()->constrained('medias');
+            $table->foreignId('image_id')->nullable()->constrained('medias');
+            $table->text('content');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('speeches');
+    }
+};
