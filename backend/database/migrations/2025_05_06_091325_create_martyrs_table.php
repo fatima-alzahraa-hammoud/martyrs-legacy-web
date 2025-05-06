a6dc5a9b34c476a14c036a535f9a8fab1b45a8fa
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-06 13:54:42 +0300

    <Backend> Add: martyrs table

---
+++ backend/database/migrations/2025_05_06_091325_create_martyrs_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('martyrs', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->string('father_name')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->date('martyrdom_date')->nullable();
+            $table->string('burial_place');
+            $table->enum('status', ['martyr', 'missing', 'detained'])->default('martyr')->nullable();
+            $table->enum('marital_status', ['single', 'engaged', 'married', 'widowed'])->default('single')->nullable();
+            $table->number('nb_of_children');
+            $table->string('related_phone_nb');
+            $table->foreign('user_id_publish')->references('id')->on('users');
+            $table->boolean('is_published');
+            $table->boolean('updating');
+            $table->boolean('is_updated');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('martyrs');
+    }
+};
