e672230f75226190e116886ca563560155f105aa
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-06 19:05:56 +0300

    <Backend> Fix: users table

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string("phone_number");
-            $table->foreignId('role_id')->constrained('role');
             $table->rememberToken();
             $table->timestamps();
         });
