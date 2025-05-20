be2231472cb5f0dec72082ba891777cdb394c1a0
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-20 11:28:16 +0300

    <Backend> Add: relations Speech model

--- backend/app/Models/Speech.php
+++ backend/app/Models/Speech.php
@@ -9,4 +9,13 @@ class Speech extends Model
     protected $fillable=[
         'martyr_id','user_id','title','description','document_type','video_id','audio_id','image_id','content','data'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function martyr()
+    {
+        return $this->belongsTo(Martyr::class);
+    }
 }
