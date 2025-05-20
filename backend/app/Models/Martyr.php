9aa7dde43750e718879d38c9197122382cc6f28d
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-20 11:33:48 +0300

    <Backend> Add: relation to Martyr model

--- backend/app/Models/Martyr.php
+++ backend/app/Models/Martyr.php
@@ -8,4 +8,9 @@ class Martyr extends Model
 {
     protected $fillable = ['first_name', 'last_name','mother_name','father_name','birth_date','martyrdom_date','burial_place','status','marital_status','nb_of_children','related_phone_nb','is_published','updating','is_updated','user_id_publish','image_id'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
