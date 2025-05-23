fd647796bb038626c4abc62d552cc38848508922
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-23 14:56:02 +0300

    <Backend> Add: getUser API

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -14,4 +14,19 @@ class UserController extends Controller
             'data' => $users
         ]);
     }
+
+    public function getUser($id){
+        $user = User::findOrFail($id);
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ]);
+    }
 }
