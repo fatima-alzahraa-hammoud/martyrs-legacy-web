960ca886fed10abde40518fcc2d46f35d7ceb87e
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-28 15:38:36 +0300

    <Backend> Add: EditorMiddleware

---
+++ backend/app/Http/Middleware/EditorMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EditorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if($request->user() && $request->user()->role_id === 2){
+            return $next($request);
+        }
+        return response()->jason(['error' => 'Unauthorized'], 403);
+
+    }
+}
