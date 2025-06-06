e3b351e867ace3b4065e19b76a927120b6721cde
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-07 00:55:11 +0300

    <Frontend> Add: Login Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 const App: React.FC = () => {
     return(
-        <div></div>
+        <Routes>
+            <Route path="/" element={<Login />} />  
+        </Routes>
     );
 }
 
