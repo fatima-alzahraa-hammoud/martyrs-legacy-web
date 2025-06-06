3fcbff8e610a8f1e840144cae9d026fcda200ed1
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-07 02:31:26 +0300

    <Frontend> Add: call login API

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
+import { requestApi } from "../utils/requestAPI";
+import { requestMethods } from "../utils/requestMethod";
 
 const Login: React.FC = () => {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [form, setLoginForm] = useState({
+        email: "",
+        password: "",
+    });
 
 
-    const login = (e: { preventDefault: () => void; }) =>{ 
+    const login = async(e: { preventDefault: () => void; }) =>{ 
         e.preventDefault(); 
         console.log("Login function called");
-        console.log("email: ", email);
-        console.log("password: ", password)
+
+        try {
+            const response = await requestApi({
+                route: "auth/login",
+                method: requestMethods.POST,
+                body: JSON.stringify(form),
+            });
+            console.log(response.data.message);
+            
+        } catch (error : any) {
+            
+        }
     }
 
     return(
@@ -19,11 +33,26 @@ const Login: React.FC = () => {
             <form onSubmit={(e) => login(e)}>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Enter Your Email"/>
+                    <input 
+                        type="email" id="email" name="email" 
+                        onChange={
+                            (e) => setLoginForm((prev) => {
+                                return { ...prev, email: e.target.value };
+                            })
+                        } 
+                        required placeholder="Enter Your Email"
+                    />
                 </div>
                 <div>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Enter Your Password"/>
+                    <input 
+                        type="password" id="password" name="password" 
+                        onChange={
+                            (e) => setLoginForm((prev) =>{
+                                return {...prev, password: e.target.value};
+                            })
+                        } 
+                        required placeholder="Enter Your Password"/>
                 </div>
                 <button type="submit">Login</button>
             </form>
