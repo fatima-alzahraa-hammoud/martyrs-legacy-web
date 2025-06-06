ba9874910959343ecf764756d994043e7d793bf5
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-06 23:26:04 +0300

    <Backend> Fix: api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 });
+
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/users', [UserController::class, 'getUsers']);
