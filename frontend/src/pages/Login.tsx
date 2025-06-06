bdede35ed2c20739be585421a064180c5751b448
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-07 01:36:13 +0300

    <Frontend> Add: content to Login Component

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login: React.FC = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const login = (e: { preventDefault: () => void; }) =>{ 
+        e.preventDefault(); 
+        console.log("Login function called");
+        console.log("email: ", email);
+        console.log("password: ", password)
+    }
+
     return(
-        <div></div>
+        <div>
+            <h1>Welcome Back!</h1>
+            <form onSubmit={(e) => login(e)}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Enter Your Email"/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Enter Your Password"/>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
     );
 }
 
