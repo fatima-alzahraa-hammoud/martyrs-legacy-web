bcaff333520b11adf08e37324799f152f94349c1
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-06 17:30:08 +0300

    <Backend> Add: register API

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -2,12 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
+
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'phone_number' => 'required|string|max:17',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        try {
+            $data = $validator->validated();
+            $data['password'] = bcrypt($data['password']);
+            $user = User::create($data);
+            $token = JWTAuth::fromUser($user);
+
+            return response()->json([
+                "status" => 'success',
+                "message" => 'User registered successfully',
+                "user" => $user,
+                "token" => $token
+            ], status: 201);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Registration failed: ' . $e->getMessage()
+            ], 500);
+        }
+    }
     public function login(Request $request){
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
