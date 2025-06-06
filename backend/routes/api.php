51c19fad5ba6d5fe9aad5ee7555ab69fe298b379
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-06 18:43:20 +0300

    <Backend> Add: register route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Database\Factories\UserFactory;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+});
 
 Route::get('/users', [UserController::class, 'getUsers']);
