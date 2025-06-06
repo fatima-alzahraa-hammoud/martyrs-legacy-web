48977441195ba5b4a054fb4aaef1aad27722fe5b
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-06 16:53:27 +0300

    <Backend> Add: Login API

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -4,8 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 422);
+        }
+        
+        $credentials = $validator->validated();
+
+        try{
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Invalid credentials'
+                ], 401);
+            }
+
+            $user = JWTAuth::user();
+
+            return response()->json([
+                "status" => 'success',
+                "message" => 'Login successful',
+                "user" => $user, 
+                "token" => $token
+            ], status: 200);
+
+        }catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Login failed: ' . $e->getMessage()
+            ], 500);
+        }
+        
+    }
 }
