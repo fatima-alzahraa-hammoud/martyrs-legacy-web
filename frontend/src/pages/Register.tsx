b0af806f463efdbe4f50997607b31e5c5d76bf85
RayhanaKheireddine <101220983@mu.edu.lb>
2025-06-07 16:43:21 +0300

    <Frontend: Add: basic register form

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { requestApi } from "../utils/requestAPI";
+import { requestMethods } from "../utils/requestMethod";
+const Register: React.FC = () => {
+
+       return(
+        <div>
+            <h1>Create a New Account</h1>
+            <form onSubmit={(e) => Register(e)}>
+              <div>
+                    <label htmlFor="name">Name:</label>
+                    <input 
+                        type="name" id="name" name="name" 
+                        onChange={
+                            (e) => setRegisterForm((prev) => {
+                                return { ...prev, name: e.target.value };
+                            })
+                        } 
+                        required placeholder="Enter Your Name"
+                    />
+                </div>
+              <div>
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                        type="email" id="email" name="email" 
+                        onChange={
+                            (e) => setRegisterForm((prev) => {
+                                return { ...prev, email: e.target.value };
+                            })
+                        } 
+                        required placeholder="Enter Your Email"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                        type="password" id="password" name="password" 
+                        onChange={
+                            (e) => setRegisterForm((prev) =>{
+                                return {...prev, password: e.target.value};
+                            })
+                        } 
+                        required placeholder="Enter Your Password"/>
+                </div>
+                <div>
+                    <label htmlFor="phone_number">Phone Number:</label>
+                    <input 
+                        type="integer" id="phone_number" name="phone_number" 
+                        onChange={
+                            (e) => setRegisterForm((prev) =>{
+                                return {...prev, phone_number: e.target.value};
+                            })
+                        } 
+                        required placeholder="Enter Your Phone Number"/>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+    
+export default Register;
+
+
+
