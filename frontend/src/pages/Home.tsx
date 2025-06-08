28ba6a8556e38df48a5b5168a6952d2b8e0d1e2c
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-08 12:28:26 +0300

    <Frontend> Add: header section to Home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+type Martyr = {
+        name: string;
+        description: string;
+};
+
 const Home: React.FC = () => {
+
+    const [martyrs, setMartyrs] = React.useState<Martyr[]>([]);
 
     return(
         <div>
+
+            {/* nav bar */}
             <nav>
                 <ul>
                     <li><a href="">الصفحة الرئيسيّة</a></li>
@@ -16,6 +25,18 @@ const Home: React.FC = () => {
                 <button>تسجيل الدّخول</button>
                 <button>إنشاء حساب جديد</button>
             </nav>
+
+            {/* Header Section */}
+            <header>
+                <p>إرث الشّهداء</p>
+                <p>هم السابقون ونحن على الدرب، نستلهم منهم الإيمان والثبات والتضحية</p>
+
+                <div>
+                    <img src="" alt="الشّهداء" />
+                </div>
+            </header>
+
+            
         </div>
     );
 }
