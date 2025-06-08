28d83c3c72b985e7ea7050750da6616a40681d3c
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-08 12:45:07 +0300

    <Frontend> Add: nav to NavBar Component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -12,20 +12,6 @@ const Home: React.FC = () => {
     return(
         <div>
 
-            {/* nav bar */}
-            <nav>
-                <ul>
-                    <li><a href="">الصفحة الرئيسيّة</a></li>
-                    <li><a href="">الشهداء</a></li>
-                    <li><a href="">الأمين على قلوبنا</a></li>
-                    <li><a href="">صفحتي</a></li>
-                    <li><a href="">تواصل معنا</a></li>
-                </ul>
-
-                <button>تسجيل الدّخول</button>
-                <button>إنشاء حساب جديد</button>
-            </nav>
-
             {/* Header Section */}
             <header>
                 <p>إرث الشّهداء</p>
--- frontend/src/pages/NavBar.tsx
+++ frontend/src/pages/NavBar.tsx
@@ -4,7 +4,19 @@ const NavBar: React.FC = () => {
 
     return(
         <div>
-            
+            {/* nav bar */}
+            <nav>
+                <ul>
+                    <li><a href="">الصفحة الرئيسيّة</a></li>
+                    <li><a href="">الشهداء</a></li>
+                    <li><a href="">الأمين على قلوبنا</a></li>
+                    <li><a href="">صفحتي</a></li>
+                    <li><a href="">تواصل معنا</a></li>
+                </ul>
+
+                <button>تسجيل الدّخول</button>
+                <button>إنشاء حساب جديد</button>
+            </nav>
         </div>
     );
 }
