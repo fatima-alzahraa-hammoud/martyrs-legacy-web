cf9b9c45ed05f18e81e283849655840692e90149
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-08 11:53:28 +0300

    <Frontend> Add: Register Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App: React.FC = () => {
     return(
         <Routes>
             <Route path="/" element={<Login />} />  
+            <Route path="/register" element={<Register />} />  
         </Routes>
     );
 }
