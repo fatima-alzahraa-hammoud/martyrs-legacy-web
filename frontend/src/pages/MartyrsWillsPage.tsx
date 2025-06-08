33f06ed855c665934eab64f8dd52bf18b3d2d314
RayhanaKheireddine <101220983@mu.edu.lb>
2025-06-08 20:48:03 +0300

    <Frontend> Add: Create basic MartyrsWillsPage

---
+++ frontend/src/pages/MartyrsWillsPage.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+type Will = {
+  id: number;
+  martyr_id:number;
+  date:number;
+};
+
+const MartyrsWillsPage: React.FC = () => {
+  const [wills, setWills] = React.useState<Will[]>([
+  ]);
+
+  return (
+    <div>
+      <Sidebar />
+      <div >
+       
+        <header>
+          <h1>وصايا الشّهداء</h1>
+          <p>كلماتهم الأخيرة أمانة في أعناقنا...</p>
+        </header>
+
+    
+        <div>
+          <button>إضافة وصيّة</button>
+          <button>بحث عن وصيّة</button>
+        </div>
+
+
+        <div>
+          {wills.map((will) => (
+            <div key={will.id} >
+              <h3> اسم الشّهيد: </h3>
+              <p>تاريخ الاستشهاد: </p>
+              <p>تاريخ الوصيّة: </p>
+
+              <p>نص الوصيّة: </p>
+              
+              <button>قراءة كاملة</button>
+            </div>
+          ))}
+        </div>
+
+        {/* Footer */}
+        <footer>
+          <p>وصايا الشّهداء، دربنا للنصر والثبات.</p>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default MartyrsWillsPage;
