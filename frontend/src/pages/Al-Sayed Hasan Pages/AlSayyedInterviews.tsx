809b1061584181a7299d5af4928e3ae580092bca
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-09 21:18:35 +0300

    <Frontend> Add: AlSayyedInterviews Component

---
+++ frontend/src/pages/Al-Sayed Hasan Pages/AlSayyedInterviews.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AlSayyedInterviews: React.FC = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AlSayyedInterviews;
