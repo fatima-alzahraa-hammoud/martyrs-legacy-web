eebf318b39661906515720691063d7bb49e5ae7a
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-12 00:52:48 +0300

    <Backend> Fix: Martyr Model

--- backend/app/Models/Martyr.php
+++ backend/app/Models/Martyr.php
@@ -6,7 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Martyr extends Model
 {
-    protected $fillable = ['first_name', 'last_name','mother_name','father_name','birth_date','martyrdom_date','burial_place','status','marital_status','nb_of_children','related_phone_nb','is_published','updating','is_updated','user_id_publish','image_id'];
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'mother_name',
+        'father_name',
+        'birth_date',
+        'place_of_birth',
+        'martyrdom_date',
+        'burial_place',
+        'description',
+        'bio',
+        'famous_quote',
+        'status',
+        'marital_status',
+        'nb_of_children',
+        'related_phone_nb',
+        'is_published',
+        'updating',
+        'is_updated',
+    ];
 
     public function user()
     {
