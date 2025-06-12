b7fdac0baebd696eebf6097bdcf10612b287e79f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-12 23:16:33 +0300

    <Backend> Add: getMartyrStories route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -26,5 +26,6 @@ Route::prefix('martyrs')->group(function () {
 });
 
 Route::prefix('martyr')->group(function () {
-    Route::get('/{id}/stories', [MartyrController::class, 'getMartyr']);
+    Route::get('/{id}', [MartyrController::class, 'getMartyr']);
+    Route::get('/{id}/stories', [MartyrController::class, 'getMartyrStories']);
 });
