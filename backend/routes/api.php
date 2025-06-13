0c3dad6a6374f364abf0bee2f9fb389595dde61f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-13 07:39:20 +0300

    <Backend> Delete: one logout route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,7 +13,6 @@ Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::middleware('jwt')->post('/logout', [AuthController::class, 'logout']);
-    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::get('/users', [UserController::class, 'getUsers']);
